063fb80b4c59367fb7b084f3b61af702fb4b7327
Ed Harcourt <edharcourt@gmail.com>
2023-11-24 15:35:25 -0500

    firestore example

---
+++ python/firestore_example/firestore_example.py
@@ -0,0 +1,58 @@
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+import json
+
+cred = credentials.Certificate("C:/Users/ehar/OneDrive - St. Lawrence University/CS345Fall23/" +
+                               "cs345fall23-firebase-adminsdk-sw4jj-5d4fa257cc.json")
+
+firebase_admin.initialize_app(cred)
+
+db = firestore.client()
+
+students = db.collection(u'students')
+
+"""
+docs = students.stream()
+for doc in docs:
+    print(f'{doc.id} => {doc.to_dict()}')
+"""
+
+# A collection is a collection of documents
+doc = students.document(u'student1')
+
+student1 = \
+    {
+        "courses": {
+
+            "c1" : {
+                "coursenum": "H101",
+                "title": "Herbology",
+                "semester": "Spring",
+                "year": 2009
+            },
+
+            "c2" :{
+                "coursenum": "C101",
+                "title": "Charms",
+                "semester": "Spring",
+                "year": 2008
+            }
+        },
+        "name": {"first": "Ron", "last": "Weasley"}
+    }
+
+#doc.set(student1)
+
+s = json.dumps(student1, indent=4)
+print(s)
+
+
+# Update a document
+doc.update({u'name.first' : u'Ronald'})
+
+# Delete a document
+#doc.delete()
+
+
+
